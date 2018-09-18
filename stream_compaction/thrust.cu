271a6485cd0016017265ce75735e947ac740d031
Uchanka <grillnov@gmail.com>
2018-09-18 00:34:09 -0400

    Turns out using thrust is so simple

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -19,9 +19,11 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
+			thrust::device_vector<int> dv_in(idata, idata + n);
+			thrust::device_vector<int> dv_out(odata, odata + n);
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
             timer().endGpuTimer();
         }
     }
